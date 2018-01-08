d8a4c5f69697a6ed614858b0d2410ec1f45e85e9
Jimmy Cozza <moatcozza@gmail.com>
2018-01-07 21:55:42 -0700

    little exception handling

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,4 +1,22 @@
 <?php
 declare(strict_types = 1);
 
-echo 'Hello World';
+namespace App;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e) {
+        echo 'Whoops.  Something went wrong';
+    });
+}
+$whoops->register();
+
+throw new \Exception;
